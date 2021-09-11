00d3aedcd475800f2067851070a26ed744884409
Lrd134 <larryc3200@gmail.com>
2021-09-11 12:45:59 -0400

    commit tree 0d9635c8e075bb1da125afb1508feda748e28bc5

--- main.js
+++ main.js
@@ -3,9 +3,51 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorDiv = document.getElementById("modal");
+const likeBtns = document.getElementsByClassName("like-glyph");
+
+const addHiddenClassToErrorDiv = () => {
+  errorDiv.classList.add("hidden");
+}
+
+const removeHiddenClassFromErrorDiv = () => {
+  errorDiv.classList.remove("hidden");
+  setTimeout(addHiddenClassToErrorDiv, 3000);
+}
+
+const handleError = (error) => {
+  errorDiv.children[1].innerText = error;
+  removeHiddenClassFromErrorDiv();
+}
 
 
-
+for (let i = 0; i < likeBtns.length; i++) {
+  likeBtns[i].addEventListener('click', () => {
+    const target = event.currentTarget;
+    let promise = mimicServerCall("http://mimicServer.example.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"      
+      }
+    }).then(resp => {
+    if (target.innerText === FULL_HEART)
+    {
+      target.innerText = EMPTY_HEART;
+      target.classList.remove("activated-heart");
+    }
+    else if (target.innerText === EMPTY_HEART)
+    {
+      target.innerText = FULL_HEART;
+      target.classList.add("activated-heart");
+    }
+    }).catch(error => {
+      handleError(error);
+    })
+  });
+}
+handleError();
+//finish error handling
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -23,3 +65,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+addHiddenClassToErrorDiv();
